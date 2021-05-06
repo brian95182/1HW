3830d7f7952f9c2df08a678dd763e0199c82d1db
容喆 王 <107111203@mail.oit.edu.tw>
2021-05-07 01:37:53 +0800

    1523

---
+++ 1HW/Prime.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _1HW
+{
+    public partial class Prime : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            int num = 9;
+            bool isPrime = true;
+            if (num != 1)
+            {
+                for (int i = 2; i < num; i++)
+                {
+                    if (num % i == 0)
+                    {
+                        isPrime = false;
+                    }
+                }
+            }
+            if (isPrime)
+            {
+                Response.Write("YES");
+            }
+            else
+            {
+                Response.Write("NO");
+            }
+
+        }
+    }
+}
